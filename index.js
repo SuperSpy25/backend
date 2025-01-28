5eaeb96d0c91952d0249b745c7a39c068ee2203f
Caleb <jenkorbjanke@gmail.com>
2025-01-28 13:33:40 -0500

    IM DONE WRITING MESSAGESgit add .git add .git add .

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ app.get("/Im_Spy", (req, res) => {
 
 app.post("/path", (req, res) => {
   console.log(req.body);
-  res.json({message: "SuperSpy, " + req.body.name});
+  res.json({LETS_COUNT_HEHEHE: fizzbuzz(req.body.numbers)});
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+function fizzbuzz(numbers) {
+  return numbers;
+}
