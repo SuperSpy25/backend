c733f75aa7df4acbb1a2fbc581b9387313d95a56
Caleb <jenkorbjanke@gmail.com>
2025-01-28 13:01:02 -0500

    why am I still naming these

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
-  res.json({hello: 'spy'});
+app.use(express.json());
+
+app.get("/Im_Spy", (req, res) => {
+  res.json({hello: "spy"});
+});
+
+app.post("/path", (req, res) => {
+  console.log(req.body);
+  res.json({message: "SuperSpy, " + req.body.name});
 });
 
 app.listen(port, () => {
